66450611d46f1b42a57f33f1993f786a6c2cba5a
Justin Sniesak <justindsniesak@gmail.com>
2025-11-05 20:24:02 -0800

    Adding CLI Go App - future expansion planned

---
+++ Code/salesTaxWesternWa.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func calcSeattleSalesTax(r float64) float64 {
+	return .1035 * r
+}
+
+func calcKingCountySalesTax(r float64) float64 {
+	return .1010 * r
+}
+
+func calcEverettSalesTax(r float64) float64 {
+	return .0990 * r
+}
+
+func calcSnahomishCountySalesTax(r float64) float64 {
+	return .1035 * r
+}
+
+func calcTacomaSalesTax(r float64) float64 {
+	return .1030 * r
+}
+
+func calcPierceCountySalesTax(r float64) float64 {
+	return .1025 * r
+}
+
+func printCost(cost float64, calcTax func(r float64) float64) {
+	taxCollected := calcTax(cost)
+	fmt.Print("The tax on your purchase is $", taxCollected, "\n")
+}
+
+func taxFunction(location int) func(r float64) float64 {
+	locationTax := map[int]func(r float64) float64{
+		1: calcSeattleSalesTax,
+		2: calcKingCountySalesTax,
+		3: calcEverettSalesTax,
+		4: calcSnahomishCountySalesTax,
+		5: calcTacomaSalesTax,
+		6: calcPierceCountySalesTax,
+	}
+	return locationTax[location]
+}
+
+func main() {
+	var cost float64
+	var location int
+
+	fmt.Print("Please enter the cost of your purchase, in dollars and cents: ")
+	fmt.Scanf("%f\n", &cost)
+	fmt.Printf("Please enter the location you want to calculate sales tax for on a purchase \n 1 - Seattle \n 2 - King \n 3 - Everett \n 4 - Snahomish \n 5- Tacoma \n 6 - Pierce: ")
+	fmt.Scanf("%d\n", &location)
+
+	printCost(cost, taxFunction(location))
+}
