cc97d27175ef0fb0dd21a0e3dfb3faee786acef0
Justin Sniesak <justindsniesak@gmail.com>
2025-11-08 21:23:13 -0800

    Update utcTimeDate.go

--- Code/utcTimeDate.go
+++ Code/utcTimeDate.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"time"
+        "fmt"
+        "time"
 )
 
 func greetingTimeDate() {
-	greeting := "Hello, "
-	currentTime := time.Now()
-	dateFormat := currentTime.Format("01/02/2006")
-	timeFormat := currentTime.Format("03:04 PM")
-	fmt.Println(greeting + "the date is " + dateFormat + ", and the time is " + timeFormat + ".")
+        greeting := "Hello!"
+        currentTime := time.Now()
+        dateFormat := currentTime.Format("01/02/2006")
+        timeFormat := currentTime.Format("03:04PM")
+        fmt.Println(greeting + ", the current date is " + dateFormat + ", and the current UTC time is " + timeFormat + ".")
 }
 
 func main() {
-	greetingTimeDate()
+        greetingTimeDate()
 }
